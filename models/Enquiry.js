ddee8c55680708e3fa6b038da2709422664e8661
JC Ivancevich <jc.ivancevich@gmail.com>
2015-07-11 18:19:12 -0300

    initial commit

---
+++ models/Enquiry.js
@@ -0,0 +1,24 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Enquiry Model
+ * =============
+ */
+
+var Enquiry = new keystone.List('Enquiry', {
+	nocreate: true,
+	noedit: true
+});
+
+Enquiry.add({
+	name: { type: Types.Name, required: true },
+	email: { type: Types.Email, required: true },
+	phone: { type: String },
+	message: { type: Types.Markdown, required: true },
+	createdAt: { type: Date, default: Date.now }
+});
+
+Enquiry.defaultSort = '-createdAt';
+Enquiry.defaultColumns = 'name, email, createdAt';
+Enquiry.register();
---
+++ routes/views/contact.js
@@ -0,0 +1,38 @@
+var keystone = require('keystone');
+var Enquiry = keystone.list('Enquiry');
+
+exports = module.exports = function(req, res) {
+	
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+	
+	// Set locals
+	locals.section = 'contact';
+	locals.formData = req.body || {};
+	locals.validationErrors = {};
+	locals.enquirySubmitted = false;
+	
+	// On POST requests, add the Enquiry item to the database
+	view.on('post', { action: 'contact' }, function(next) {
+		
+		var newEnquiry = new Enquiry.model(),
+			updater = newEnquiry.getUpdateHandler(req);
+		
+		updater.process(req.body, {
+			flashErrors: true,
+			fields: 'name, email, phone, message',
+			errorMessage: 'Hubo un problema enviando su mensaje:'
+		}, function(err) {
+			if (err) {
+				locals.validationErrors = err.errors;
+			} else {
+				locals.enquirySubmitted = true;
+			}
+			next();
+		});
+		
+	});
+	
+	view.render('contact');
+	
+};
