6a51c4d83ce0a21cb5d29d305898b1dae8e85312
JC Ivancevich <jc.ivancevich@gmail.com>
2016-02-04 14:47:10 -0300

    add possibility to upload prescription

--- models/Enquiry.js
+++ models/Enquiry.js
@@ -1,6 +1,8 @@
 'use strict';
-
+var fs = require('fs');
 var keystone = require('keystone');
+var fileType = require('file-type');
+var readChunk = require('read-chunk');
 var Types = keystone.Field.Types;
 
 /**
@@ -17,6 +19,7 @@ Enquiry.add({
   name: { type: Types.Name, required: true },
   email: { type: Types.Email, required: true },
   phone: { type: String },
+  prescription: { type: Types.Url },
   message: { type: Types.Markdown, required: true },
   createdAt: { type: Date, 'default': Date.now }
 });
@@ -30,23 +33,53 @@ Enquiry.schema.methods.sendEmail = function() {
   keystone.list('User').model.find().where('isAdmin', true).exec(function (err, admins) {
     if (err) return console.error('Error getting admins', err);
 
-    var Email = new keystone.Email('enquiry-notification');
+    getPrescription(enquiry, function(err, prescription) {
+      var Email = new keystone.Email('enquiry-notification');
+      var attachments = [];
 
-    Email.send({
-        subject: 'Consulta desde la web de Alergo-Pharma',
-        tags: 'Enquiry',
-        to: admins,
-        from: {
-          name: enquiry.name.first + (enquiry.name.last ? (' ' + enquiry.name.last) : ''),
-          email: enquiry.email
-        },
-        enquiry: enquiry
-    }, function (err, info) {
-      if (err) {
-        console.error('Error sending enquiry email', err);
+      if (prescription) {
+        attachments.push(prescription);
       }
+
+      Email.send({
+          subject: 'Consulta desde la web de Alergo-Pharma',
+          tags: 'Enquiry',
+          to: admins,
+          from: {
+            name: enquiry.name.first + (enquiry.name.last ? (' ' + enquiry.name.last) : ''),
+            email: enquiry.email
+          },
+          attachments: attachments,
+          enquiry: enquiry
+      }, function (err, info) {
+        if (err) {
+          console.error('Error sending enquiry email', err);
+        }
+      });
     });
+
   });
 };
 
 Enquiry.register();
+
+function getPrescription(enquiry, callback) {
+  if (!enquiry.prescription) {
+    return callback();
+  }
+
+  fs.readFile(enquiry.prescription, 'base64', function (err, data) {
+    if (err) {
+      return callback(err);
+    }
+
+    var buffer = readChunk.sync(enquiry.prescription, 0, 262);
+    var type = fileType(buffer) || {};
+
+    callback(null, {
+      type: type.mime,
+      name: 'prescripcion.' + type.ext,
+      content: data,
+    });
+  });
+}
--- routes/views/contact.js
+++ routes/views/contact.js
@@ -1,42 +1,105 @@
 'use strict';
-
+var fs = require('fs');
+var _ = require('underscore');
 var keystone = require('keystone');
 var Enquiry = keystone.list('Enquiry');
 
 exports = module.exports = function (req, res) {
-  
   var view = new keystone.View(req, res);
   var locals = res.locals;
-  
+
   // Set locals
   locals.section = 'contact';
   locals.title = 'Alergo Pharma - Contacto';
   locals.formData = req.body || {};
   locals.validationErrors = {};
   locals.enquirySubmitted = false;
-  
+
   // On POST requests, add the Enquiry item to the database
   view.on('post', { action: 'contact' }, function (next) {
-    
-    var newEnquiry = new Enquiry.model(),
-      updater = newEnquiry.getUpdateHandler(req);
-    
-    updater.process(req.body, {
-      flashErrors: true,
-      fields: 'name, email, phone, message',
-      errorMessage: 'Hubo un problema enviando su mensaje:'
-    }, function (err) {
+
+    uploadFile(req, 'prescription', function (err, filePath) {
       if (err) {
-        locals.validationErrors = err.errors;
-      } else {
-        locals.enquirySubmitted = true;
-        newEnquiry.sendEmail();
+        req.flash('error', {
+          type: 'ValidationError',
+          title: 'Hubo un problema enviando su mensaje:',
+          list: _.pluck(err.errors, 'message')
+        });
+        return next();
       }
-      next();
+
+      req.body.prescription = filePath;
+
+      var newEnquiry = new Enquiry.model();
+      var updater = newEnquiry.getUpdateHandler(req);
+
+      updater.process(req.body, {
+        flashErrors: true,
+        fields: 'name, email, phone, prescription, message',
+        errorMessage: 'Hubo un problema enviando su mensaje:'
+      }, function (err) {
+        if (err) {
+          locals.validationErrors = err.errors;
+        } else {
+          locals.enquirySubmitted = true;
+          newEnquiry.sendEmail();
+        }
+        next();
+      });
+      
     });
-    
+
   });
   
   view.render('contact');
-  
+
 };
+
+function uploadFile(req, fileName, callback) {
+  if (!req.files || !req.files[fileName]) {
+    return callback();
+  }
+
+  var file = req.files[fileName];
+  var path = file.path;
+
+  var tenMB = 10000000;
+
+  if (file.size > tenMB) {
+    return callback(getError('La prescripción no puede superar los 10MB'));
+  }
+
+  fs.readFile(path, function (err, data) {
+    if (err) {
+      console.error(err);
+      return callback(getError('Error al cargar prescripción'));
+    }
+
+    var newPath = process.cwd() + '/uploads/' + file.name;
+
+    fs.writeFile(newPath, data, function (err) {
+      if (err) {
+        console.error(err);
+        return callback(getError('Error al cargar prescripción'));
+      }
+
+      callback(null, newPath);
+    });
+
+  });
+}
+
+function getError(message) {
+  return {
+    message: 'Validation failed',
+    name: 'ValidationError',
+    errors: {
+      prescription: {
+        name: 'ValidatorError',
+        path: 'prescription',
+        message: message,
+        type: 'required'
+      }
+    }
+  }
+}
